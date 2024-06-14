f2fdd22d637e70b57e467e4e1212b71179b9d79f
SiRiOS <sirios@sirios.com.br>
2024-06-14 15:54:52 -0300

    feat: config webpack e add babel

---
+++ src/main.js
@@ -0,0 +1,9 @@
+"use strict"
+
+import "./styles/global.css"
+import "./styles/form.css"
+import "./styles/card.css"
+import "./styles/colors.css"
+import "./styles/content.css"
+import "./styles/header.css"
+import "./styles/typography.css"
